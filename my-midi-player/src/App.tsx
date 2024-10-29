19157e24f5f86f938e89784bbf47e717f142dc2d
Omar Rios <omarrx14@gmail.com>
2024-10-29 01:25:41 -0700

    Update App.tsx

--- my-midi-player/src/App.tsx
+++ my-midi-player/src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LofiStation from './components/lofistation/lofistation.tsx';
 import RetroMIDIReaderPlayer from './components/lofistation/lofistation2.tsx';
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/lofistation" Component={LofiStation} />
-        <Route path="/lofistation2" Component={RetroMIDIReaderPlayer} />
-
+        <Route path="/lofistation2" element={<RetroMIDIReaderPlayer />} />
         {/* Otras rutas */}
       </Routes>
     </Router>
