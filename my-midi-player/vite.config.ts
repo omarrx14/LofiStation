e681bcee7523015108156000d6f84072c87893a5
Omar Rios <omarrx14@gmail.com>
2024-10-29 01:34:27 -0700

    fixprod

--- my-midi-player/vite.config.ts
+++ my-midi-player/vite.config.ts
@@ -1,12 +1,13 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensure this points to the root
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
